dd3828766d85c23b4c30ed4d0fd3a35575c64277
Samyak Manandhar <sm20agc@herts.ac.uk>
2023-09-14 15:26:47 +0100

    Basic Navigation Loading

---
+++ app/src/main/java/com/example/clickermain/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.clickermain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.clickermain.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
---
+++ app/src/main/java/com/example/clickermain/StartFragment.kt
@@ -0,0 +1,35 @@
+package com.example.clickermain
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.clickermain.databinding.FragmentStartBinding
+
+
+class StartFragment : Fragment() {
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.test.setOnClickListener {
+            val action = StartFragmentDirections.actionStartFragmentToHomeFragment()
+            view.findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
